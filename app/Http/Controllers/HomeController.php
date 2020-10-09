d007e981ba5ee36dae20d81a18bc961686321729
Pxndx-user007 <cmerino@unitru.edu.pe>
2020-10-09 00:26:17 -0500

    monthly sales chart finished

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Employee;
 use App\Models\User;
 use Auth;
+use DB;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -29,7 +30,6 @@ class HomeController extends Controller
         $page_name = "Home";
         $page_subpage = "Dashboard";
         $page_icon ="fa fa-home";
-        
         $auth = Auth::user();
         $employees = Employee::all();
         foreach ($employees as $key) {
@@ -37,9 +37,24 @@ class HomeController extends Controller
                 $user = $key;
             }
         }
-
-        return view('home', compact("user", "page_name","page_subpage", "page_icon"));
+        $id = $auth->id;
+        $data = DB::select('CALL monthly_sales('.$id.')');
+        return view('home', compact("user", "data", "page_name","page_subpage", "page_icon"));
     }
 
- 
+    // public function chart(Request $request)
+    // {
+    //     $auth = Auth::user();
+    //     $id = $auth->id;
+    //     $mes = date("m");
+    //     $data = array();
+       
+    //     $data = DB::select('CALL monthly_sales('.$id.')');
+        
+       
+    //     // return $data;
+  
+    //     return response(json_encode($data),200)->header('Content-type', 'text/plain');
+    // }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,100 +1,118 @@
-@extends('layout.template') 
+@extends('layout.template')
 
 @section('content')
 <div class="row d-flex justify-content-center">
     @if ($user->workstation->work == "ADMINISTRATOR")
-        <div class="col-md-10">
-            <div class="tile">
-                <h3 class="tile-title">Monthly Sales</h3>
-                <div class="embed-responsive embed-responsive-16by9">
-                    <canvas class="embed-responsive-item" id="Monthly_Sales"></canvas>
-                </div>
+    <div class="col-md-10">
+        <div class="tile">
+            <h3 class="tile-title">Monthly Sales</h3>
+            <div class="embed-responsive embed-responsive-16by9">
+                <canvas class="embed-responsive-item" id="Monthly_Sales"></canvas>
             </div>
         </div>
-        <div class="col-md-10">
-            <div class="tile">
-                <h3 class="tile-title">Bar Chart</h3>
-                <div class="embed-responsive embed-responsive-16by9">
-                    <canvas class="embed-responsive-item" id="barChartDemo"></canvas>
-                </div>
+    </div>
+
+    <div class="col-md-10">
+        <div class="tile">
+            <h3 class="tile-title">Bar Chart</h3>
+            <div class="embed-responsive embed-responsive-16by9">
+                <canvas class="embed-responsive-item" id="barChartDemo"></canvas>
             </div>
         </div>
+    </div>
+    <div>
+
+    </div>
     @else
-        
+
     @endif
-   <form action="/home/dashboard" method="post" id="form-request">
+    <form action="" method="post" id="form-request">
         @csrf
         <input type="hidden" name="id" value="{{$user->id}}">
+        {{-- <input id="cargar" type="hidden" onclick='load()'> --}}
     </form>
 </div>
 @endsection
 @section('script')
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
+<script src="/js/chart/dashboard.js"></script>
 <script type="text/javascript">
+
+$(document).ready(function () {
+
     function monthlySales(ctx) {
-        new Chart(ctx, {
-            type: 'line',
-            data:{
-                labels: ["January", "February", "March", "April", "May","June", "July", "August", "October", "November", "December"],
-                datasets:[
-                    { 
-                        label: 'Sales',
-                        data: [1125, 1150, 1140, 1170, 1160, 1140, 1180 , 1160, 1170, 1150 , 1170],
-                        borderColor: 'green',
-                    }
-                ]
+
+        new Chart(ctx, 
+        {
+             type: 'line',
+            data: {
+
+                labels: [
+                    @foreach($data as $item)
+                        "{{$item->month}}",
+                    @endforeach
+                ],
+                datasets: [{
+
+                    label: 'Sales',
+                    data: [
+                        @foreach($data as $item)
+                            "{{$item->quantity}}",
+                        @endforeach
+                    ],
+                    borderColor: 'green',
+                }]
             },
-            options:{
-                scales:{
-                    xAxes:[{
-                        gridLines:{
+            options: {
+                scales: {
+                    xAxes: [{
+                        gridLines: {
                             display: false,
                         }
                     }]
-                },
-                legend:{
+                 },
+                legend: {
                     position: 'bottom',
-                    labels:{
+                    labels: {
                         padding: 20,
                         boxWidth: 25,
                         fontColor: 'black'
                     }
-                },
-                tooltips:{
+                 },
+                tooltips: {
                     backgroundColor: '#26518C',
                     xPadding: 10,
                     yPadding: 10
                 },
-                elements:{
-                    line:{
+                elements: {
+                    line: {
                         borderWidth: 4,
                         fill: false,
                     },
-                    point:{
+                    point: {
                         radius: 4,
                         borderWidth: 2,
                         backgroundColor: 'white',
                         hoverRadius: 6,
-                        hoverBorderWidth:2,
+                        hoverBorderWidth: 2,
                     }
                 }
-                
             }
         })
     }
-
     function renderChart() {
         const ctx = document.querySelector('#Monthly_Sales').getContext('2d')
         monthlySales(ctx)
     }
 
     renderChart()
+});
 </script>
 {{-- <script type="text/javascript">
     var ctxl = $("#Monthly_Sales").get(0).getContext("2d");
     var lineChart = new Chart(ctxl).Line(data);
-    
+
     var ctxp = $("#barChartDemo").get(0).getContext("2d");
     var pieChart = new Chart(ctxp).Bar(data);
-  </script> --}}
+</script> --}}
 @endsection
